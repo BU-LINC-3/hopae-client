86fad4545073d14885eeaef8d274b65fdd9a95d2
dlghgus5656 <82634597+dlghgus5656@users.noreply.github.com>
2021-06-02 00:11:26 +0900

    QRcode  생성
    입력된 내용으로 QRcode 생성 코딩

---
+++ QRcode/app/src/main/java/com/example/qrcode/qrcode.kt
@@ -0,0 +1,45 @@
+package com.example.qrcode
+
+import android.app.Activity
+import android.os.Bundle
+import android.webkit.WebView
+import android.widget.*
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val layout = LinearLayout(this)
+        layout.orientation = 1
+
+        val txt1 = TextView(this)
+        txt1.text = "내용 : "
+        txt1.textSize = 18f
+        layout.addView(txt1)
+        val txt2 = EditText(this)
+        txt2.hint = "QR 코드에 담을 내용 입력..."
+        layout.addView(txt2)
+
+        val web = WebView(this)
+        val btn = Button(this)
+        btn.text = "QRcode 생성"
+        btn.setOnClickListener {
+            val input = txt2.text.toString()
+            if (input.isBlank()) Toast.makeText(this, "입력된 내용이 없습니.", Toast.LENGTH_LONG).show()
+            else web.loadData("<img width=100% src='https://chart.apis.google.com/chart?cht=qr&chs=320x320&chl=$input'>",
+                    "text/html; charset=UTF-8", null); //input으로 입력받은 내용을 qrcode로 생성함
+        }
+        layout.addView(btn);
+        layout.addView(web);
+
+        val pad = dip2px(16)
+        layout.setPadding(pad, pad, pad, pad)
+        val scroll = ScrollView(this)
+        scroll.addView(layout)
+        setContentView(scroll)
+    }
+
+    fun dip2px(dips: Int): Int {
+        return Math.ceil((dips * resources.displayMetrics.density).toDouble()).toInt()
+    }
+}
